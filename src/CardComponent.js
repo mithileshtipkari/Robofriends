0807aecf43d34ab70c0d009f8a214dd9ecf87342
mithilesht <mithilesht@cdac.in>
2018-10-29 20:20:11 +0530

    added js code in jsx

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Card from './Card'
 
 const CardComponent = ({robots}) =>{
-	const cardsArray = robots.map((element)=>{
-		return <Card id={element.id} name={element.name} email={element.email}/>
-	});
 	return(
 		<div>
-			{cardsArray}
+			{
+				robots.map((element)=>{
+					return( 
+						<Card
+							key={element.id} 
+							id={element.id} 
+							name={element.name} 
+							email={element.email}
+						/>
+					);
+				})
+			}
 		</div>
 	);
 }
