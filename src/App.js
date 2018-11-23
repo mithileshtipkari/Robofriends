ae3b9a4a3c2ed8f4003fbf0275da4fa384e72b85
mithileshtipkari <mithilesh.tipkari@gmail.com>
2018-11-23 22:38:27 +0530

    Used component life-cycle methods

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import CardList from './CardList'
 import {robots} from './robots'//{} -> shorthand to import non default variables
 import SearchBox from './SearchBox'
 import './App.css'
 
-class App extends React.Component{
+class App extends Component{
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchField: ''
 		}
 	}
@@ -18,10 +18,20 @@ class App extends React.Component{
 		this.setState({searchField: event.target.value})
 	}
 
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+  			.then(response => {
+  				return response.json()
+  			})
+  			.then(json => {
+  				this.setState({robots: json})	
+  			})
+	}
+
 	render(){
 		const filteredRobots = this.state.robots.filter(robots =>{
 			return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-		})
+		});
 		return(
 			<div className='tc'>
 				<h1 className='f1'>RoboFriends</h1>
